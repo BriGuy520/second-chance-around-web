9eb7192ce871385ca68fb496438de42e105d8304
Brian Falasz <bfalasz@yahoo.com>
2019-06-17 21:32:15 -0500

    added a form to the contact page

---
+++ server/contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+  <form action="contact.php" method="POST">
+    <p>
+      Name:<input type="text" name="firstname" /><input type="text" name="lastname" />
+      Email:<input type="text" name="email" />
+    </p>
+    <p>Project Start Date:<input type="date" name="startdate" /></p>
+    <p>Project End Date:<input type="date" name="enddate" /></p>
+    <p>More Details:<br/></p>
+    <textarea name="details" rows="10" cols="10"></textarea>
+    </p>
+
+
+</body>
+</html>
--- src/components/contact/GeneralRequest.js
+++ src/components/contact/GeneralRequest.js
@@ -8,9 +8,18 @@ const GeneralRequest = () => {
         Not ready just yet, but still have questions? Don't sweat it. Fill out all of the relevant information below. 
         and we will get back to you ASAP. We promise.
       </p>
-      <form>
+      <form action="contact.php" method="POST">
+      <p>
+        Name:<input type="text" name="firstname" /><input type="text" name="lastname" />
+        Email:<input type="text" name="email" />
+      </p>
+      <p>Project Start Date:<input type="date" name="startdate" /></p>
+      <p>Project End Date:<input type="date" name="enddate" /></p>
+      <p>More Details:<br/>
+        <textarea name="details" rows="10" cols="100"></textarea>
+      </p>
+  </form>
 
-      </form>
     </div>
   )
 }
