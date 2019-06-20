2dd9248d4131ade9f99bb715dc0b86d737d2cb55
Brian Falasz <bfalasz@yahoo.com>
2019-06-20 12:25:36 -0500

    using react hooks to help create an animation for contact forms

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Consultation from './Consultation';
 import General from './General';
 
-const Contact = () => {
+function Contact(){
+
+  const [request, setRequest] = useState(false);
+  const [general, setGeneral] = useState(false);
+
   return (
     <div className="content">
       <h1>Contact Us</h1>
       <div>
-        <Consultation />
-        <General />
+        <div>
+          <button className="ui button black" onClick={() => setRequest(true)}>Show Form</button>
+          {request ? <Consultation /> : null}
+          
+        </div>
+        <div>
+          <button className="ui button black" onClick={() => setGeneral(true)}>Show Form</button>
+          { general ? <General /> : null} 
+        </div>
+        
       </div>
     </div>
   );
