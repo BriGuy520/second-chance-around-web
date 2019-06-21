d5e399dec43177e133aaa90d82f7fcd5e6252e00
Brian Falasz <bfalasz@yahoo.com>
2019-06-21 12:25:20 -0500

    making progress on the slide down animation for contact forms

--- src/components/About.js
+++ src/components/About.js
@@ -2,31 +2,33 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="about-section content">
+    <div className="content">
       <div className="about-cover">
         <h2>About Us</h2>
       </div>
-      <p>
-      Co-Founder and CEO Ashley began taking on personal organizing jobs when she found her day job 
-      as a nanny becoming less and less time consuming. As she began to help more and more clients with 
-      their organizing needs, she kept coming across the same problems.
-      </p>
-      <p>
-        The first was she would organize a space, return and find it in the same state
-        as it was the first time she arrived. A complete mess. This was happening because her
-        clients were attempting to store things that were not being used... Over time she honed in on
-        on her simplifying skills and crafted a list of loaded questions that helped her clients make
-        thoughful decisions about what they needed and what they didn't. And once the clients let all those
-        extra things go, the space stayed organized! And we can safely say no one has ever regretted this 
-        simplifying process.
-      </p>
-      <p>
-        Another problem Ashley discovered as an organizer is how much extra work
-        is involved with getting rid of everything once you realize you don't need it anymore...
-        There are always tons of bags and boxes to donate, trash to take out, items to put back, and most of the time items
-        that could be resold to for a little extra pocket change or to help with the expense of the organizing 
-        services in general.
-      </p>
+      <div className="about-section">
+        <p>
+        Co-Founder and CEO Ashley began taking on personal organizing jobs when she found her day job 
+        as a nanny becoming less and less time consuming. As she began to help more and more clients with 
+        their organizing needs, she kept coming across the same problems.
+        </p>
+        <p>
+          The first was she would organize a space, return and find it in the same state
+          as it was the first time she arrived. A complete mess. This was happening because her
+          clients were attempting to store things that were not being used... Over time she honed in on
+          on her simplifying skills and crafted a list of loaded questions that helped her clients make
+          thoughful decisions about what they needed and what they didn't. And once the clients let all those
+          extra things go, the space stayed organized! And we can safely say no one has ever regretted this 
+          simplifying process.
+        </p>
+        <p>
+          Another problem Ashley discovered as an organizer is how much extra work
+          is involved with getting rid of everything once you realize you don't need it anymore...
+          There are always tons of bags and boxes to donate, trash to take out, items to put back, and most of the time items
+          that could be resold to for a little extra pocket change or to help with the expense of the organizing 
+          services in general.
+        </p>
+      </div>
     </div>
   )
 }
