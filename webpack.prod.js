79d420e3abce3b3770cf9247384886f24e157db5
Brian Falasz <bfalasz@yahoo.com>
2019-06-25 12:42:29 -0500

    small mod to webpack prod file

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports =  merge(common, {
